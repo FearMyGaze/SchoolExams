1fb4adfc94eb84207272bc3d4785e5aecf9fcd1b
Giorgos Papoulias <FearMyGaze@outlook.com>
2022-02-16 01:08:16 +0200

    fix

--- Exam1/src/main/java/org/example/userDB.java
+++ Exam1/src/main/java/org/example/userDB.java
@@ -2,9 +2,6 @@ package org.example;
 
 public class userDB {
    public boolean changePassword(String username, String password){
-       if (username.equals("admin")){
-           return true;
-       }
-       return false;
+       return username.equals("admin");
    }
 }
--- Exam1/src/main/java/org/example/userService.java
+++ Exam1/src/main/java/org/example/userService.java
@@ -3,7 +3,7 @@ package org.example;
 public class userService {
     boolean changePassword(String username, String passwd){
         userDB user =  new userDB();
-        if (passwd.length() > 8){
+        if (passwd.length() >= 8){
             return user.changePassword(username,passwd);
         }
         return false;
